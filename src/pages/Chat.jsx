5a6ddbc5a7f901c42e337c0612e27caf97355927
dev-kishor138 <dev.kishor138@gmail.com>
2024-11-24 21:12:20 +0600

    nothing

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -2,6 +2,11 @@ const Chat = () => {
   return (
     <div>
       <h1>Chat</h1>
+      <h1>Chat</h1>
+      <h1>Chat</h1>
+      <h1>Chat</h1>
+      <h1>Chat</h1>
+      <h1>Chat</h1>
     </div>
   );
 };
